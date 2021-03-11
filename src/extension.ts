86df41d72d6f6b0dd548056f79e15600282ffec5
Jed Fox <git@jedfox.com>
2021-03-11 18:31:57 -0500

    Implement

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import spawn from "spawn-tag";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "yoink" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "yoink" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "yoink.addToYoink",
+    async (selectedURI?: vscode.Uri) => {
+      // The code you place here will be executed every time your command is executed
+      const uri = selectedURI ?? vscode.window.activeTextEditor?.document.uri;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('yoink.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      if (uri && uri.scheme == "file") {
+        spawn.silently`open -a Yoink ${uri.fsPath}`;
+      }
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Yoink!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
